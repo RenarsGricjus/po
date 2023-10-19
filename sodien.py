eb7bad3f0539c57893fd88d149f2d9201fbbea99
RenarsGricjus <renars.gricjus@grobinasskola.lv>
2023-10-19 14:26:35 +0300

    Init commit

---
+++ sodien.py
@@ -0,0 +1,8 @@
+# Lietotājam tiek prasīts vārds un termināli izvada vārdu
+
+"""print("Ievadiet vārdu:")
+x = input()
+print("Labdien, "+ x)"""
+
+a=input("Ieraksti savu vārdu: ")
+print(f"Mani sauc {a}")
